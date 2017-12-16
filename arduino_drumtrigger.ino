76d7332fee22c1e12931cfeac64cf9b579a6a2e7
ranga95 <32329046+ranga95@users.noreply.github.com>
2017-12-16 20:18:19 +0530

    Add files via upload

---
+++ arduino_drumtrigger.ino
@@ -0,0 +1,19 @@
+
+  
+int sensor;         
+
+
+void setup()
+{
+  Serial.begin(9600);      //Only for debugging
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  sensor = digitalRead(A0);
+
+    Serial.println(sensor);
+  
+  delay(200); //Small delay
+}
